43608b754b8814b9e3eddf0f9ededf8843f2c3fd
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-10-20 01:01:30 +0200

    made AIBoat class
    made testing map for pathfinding, and created an empty Pawn class for the AI Boat

---
+++ Source/CoastSim/Private/AIBoat.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIBoat.h"
+
+// Sets default values
+AAIBoat::AAIBoat()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAIBoat::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAIBoat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAIBoat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
