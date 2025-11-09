de8adab0c8814f371f52dc3b665fec4835a51f91
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-11-09 20:56:34 +0100

    copied from PK

--- Source/CoastSim/Private/AStarNode.cpp
+++ Source/CoastSim/Private/AStarNode.cpp
@@ -3,20 +3,87 @@
 
 #include "AStarNode.h"
 
-// Sets default values
-AAStarNode::AAStarNode()
+
+AAStarNode::AAStarNode(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	TileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TileMesh"));
+	SetRootComponent(TileMesh);
+
+	TileMesh->SetMobility(EComponentMobility::Static);
+	TileMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TileMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	TileMesh->SetCollisionResponseToChannel(ECC_Visibility, ECollisionResponse::ECR_Block);
+	TileMesh->bHiddenInGame = false;
+
+	
+}
+
+void AAStarNode::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!DynamicMaterial)
+	{
+		DynamicMaterial = UMaterialInstanceDynamic::Create(TileMesh->GetMaterial(0), this);
+		TileMesh->SetMaterial(0, DynamicMaterial);
+	}
+
+	SetState(bIsWalkable ? ENodeState::Default : ENodeState::Blocked);
+	CalculateFCost();
+}
+
+void AAStarNode::CalculateFCost()
+{
+	FCost = GCost + HCost;
 
 }
 
-
-
-// Called every frame
-void AAStarNode::Tick(float DeltaTime)
+void AAStarNode::ApplyColor(const FLinearColor& Color)
 {
-	Super::Tick(DeltaTime);
-
+	if (DynamicMaterial)
+	{
+		DynamicMaterial->SetVectorParameterValue(TEXT("Color"), Color);
+	}
 }
 
+void AAStarNode::SetState(ENodeState NewState)
+{
+	CurrentState = NewState;
+
+	switch (NewState)
+	{
+	case ENodeState::Default:
+		ApplyColor(FLinearColor::White);
+		break;
+	case ENodeState::Blocked:
+		ApplyColor(FLinearColor::Red);
+		break;
+	case ENodeState::Open:
+		ApplyColor(FLinearColor::Yellow);
+		break;
+	case ENodeState::Closed:
+		ApplyColor(FLinearColor::Blue);
+		break;
+	case ENodeState::Path:
+		ApplyColor(FLinearColor::Green);
+		break;
+	case ENodeState::Start:
+		ApplyColor(FLinearColor(0, 255, 255, 255));
+		break;
+	case ENodeState::End:
+		ApplyColor(FLinearColor(195, 115, 0, 255));
+		break;
+	default:
+		break;
+	}
+}
+
+void AAStarNode::NotifyActorOnClicked(FKey ButtonPressed)
+{
+	Super::NotifyActorOnClicked(ButtonPressed);
+
+	bIsWalkable = !bIsWalkable;
+	SetState(bIsWalkable ? ENodeState::Default : ENodeState::Blocked);
+}
--- Source/CoastSim/Public/AStarNode.h
+++ Source/CoastSim/Public/AStarNode.h
@@ -20,16 +20,54 @@ enum class ENodeState : uint8
 	End
 };
 
-UCLASS()
+UCLASS(BlueprintType)
 class COASTSIM_API AAStarNode : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
-	AAStarNode();
-	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	AAStarNode(const FObjectInitializer& ObjectInitializer);
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
+	void CalculateFCost();
+
+	void SetState(ENodeState NewState);
+
+	virtual void NotifyActorOnClicked(FKey ButtonPressed) override;
+
+	void ApplyColor(const FLinearColor& Color);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	int32 X = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	int32 Y = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	bool bIsWalkable = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	float TileCost = 1.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
+	float GCost = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
+	float HCost = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
+	float FCost = 0.f;
+
+	UPROPERTY()
+	AAStarNode* Parent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Components")
+	UStaticMeshComponent* TileMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="A* Node")
+	ENodeState CurrentState = ENodeState::Default;
+	
+	UPROPERTY()
+	UMaterialInstanceDynamic* DynamicMaterial;
 };
