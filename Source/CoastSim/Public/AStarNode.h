932fc4955bb96f2cb4501bd514780e0d41267655
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-11-09 21:01:55 +0100

    nvm this is MY class now, not PK

--- Source/CoastSim/Public/AStarNode.h
+++ Source/CoastSim/Public/AStarNode.h
@@ -24,50 +24,8 @@ UCLASS(BlueprintType)
 class COASTSIM_API AAStarNode : public AActor
 {
 	GENERATED_BODY()
+public:
 	
-public:	
-	AAStarNode(const FObjectInitializer& ObjectInitializer);
 
-	virtual void OnConstruction(const FTransform& Transform) override;
 	
-	void CalculateFCost();
-
-	void SetState(ENodeState NewState);
-
-	virtual void NotifyActorOnClicked(FKey ButtonPressed) override;
-
-	void ApplyColor(const FLinearColor& Color);
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
-	int32 X = 0;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
-	int32 Y = 0;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
-	bool bIsWalkable = true;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
-	float TileCost = 1.0f;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
-	float GCost = 0.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
-	float HCost = 0.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Node")
-	float FCost = 0.f;
-
-	UPROPERTY()
-	AAStarNode* Parent = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Components")
-	UStaticMeshComponent* TileMesh;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="A* Node")
-	ENodeState CurrentState = ENodeState::Default;
-	
-	UPROPERTY()
-	UMaterialInstanceDynamic* DynamicMaterial;
 };
