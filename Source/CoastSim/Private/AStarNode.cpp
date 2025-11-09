8049b3e363a91879b52c4312e3bed9b00f772d7a
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-11-09 20:50:48 +0100

    Created AStar Classes

---
+++ Source/CoastSim/Private/AStarNode.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AStarNode.h"
+
+// Sets default values
+AAStarNode::AAStarNode()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+
+
+// Called every frame
+void AAStarNode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CoastSim/Public/AStarNode.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AStarNode.generated.h"
+
+
+// Enum for showing the node state for A*
+UENUM(BlueprintType) 
+enum class ENodeState : uint8
+{
+	Default,
+	Blocked,
+	Open,
+	Closed,
+	Path,
+	Start,
+	End
+};
+
+UCLASS()
+class COASTSIM_API AAStarNode : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAStarNode();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
