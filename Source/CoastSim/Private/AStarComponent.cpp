d69b4c41813ecdecd9590595e13f7aae3b6b8227
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-11-17 20:45:38 +0100

    ops

--- Source/CoastSim/Private/AStarComponent.cpp
+++ Source/CoastSim/Private/AStarComponent.cpp
@@ -51,7 +51,7 @@ void UAStarComponent::ChangeDirection()
 	// Turns it into unit vector
 	NewDirection /= NewDirection.Length();
 	
-	
+	MovementDirection = NewDirection;
 }
 
 // Called every frame
