125c122792082d6cf8d62f9e577656b10d8ccc9c
Kirbywithacoolhat <83244774+Inkling4@users.noreply.github.com>
2025-11-30 17:31:39 +0100

    Reapply "Merge branch 'main' into AStar"
    This reverts commit abc303bc4e21933314c3ae7b608b3218a6203d35.

--- Source/CoastSim/Private/CPP_Buoyancy.cpp
+++ Source/CoastSim/Private/CPP_Buoyancy.cpp
@@ -3,6 +3,8 @@
 
 #include "CPP_Buoyancy.h"
 
+#include <gsl/pointers>
+
 // Sets default values for this component's properties
 UCPP_Buoyancy::UCPP_Buoyancy()
 {
@@ -24,9 +26,7 @@ void UCPP_Buoyancy::BeginPlay()
 	OwnerActor = GetOwner();
 	USceneComponent* rootComponent = OwnerActor->GetRootComponent();
 	
-	VisualComponent = Cast<UStaticMeshComponent>(GetDefaultSubobjectByName(TEXT("Visual")));
-	if (!VisualComponent)
-		UE_LOG(LogTemp, Error, TEXT("No Visual Component on buoyancy object"))
+	//VisualComponent = Cast<USceneComponent>(OwnerActor->GetDefaultSubobjectByName(TEXT("Visual"))); if (VisualComponent == nullptr) UE_LOG(LogTemp, Error, TEXT("No Visual Component on buoyancy object"))
 }
 
 
@@ -37,10 +37,8 @@ void UCPP_Buoyancy::TickComponent(float DeltaTime, ELevelTick TickType, FActorCo
 
 	// ...
 
-	if (VisualComponent)
-	{
 		SurfaceAveragePos();
-	}
+	
 }
 
 float UCPP_Buoyancy::SurfaceAveragePos()
@@ -48,7 +46,7 @@ float UCPP_Buoyancy::SurfaceAveragePos()
 	AActor* owner = GetOwner();
 
 	
-	FHitResult HitResult;
+	TArray<FHitResult> HitResults;
 	FVector RayOrigin = owner->GetActorLocation();
 	float RayDistance = 1000.0f;
 	FVector RayEnd = RayOrigin + FVector(0, 0, RayDistance);
@@ -59,15 +57,39 @@ float UCPP_Buoyancy::SurfaceAveragePos()
 	FCollisionQueryParams CollisionParams;
 	CollisionParams.AddIgnoredActor(owner); // Ignore own actor
 
-	bool bHit = GetWorld()->LineTraceSingleByObjectType(HitResult, RayOrigin, RayEnd, ObjectParams, CollisionParams);
+	//bool bHit = GetWorld()->LineTraceSingleByObjectType(HitResults, RayOrigin, RayEnd, ECC_GameTraceChannel1, CollisionParams);
+	bool bHit = GetWorld()->LineTraceMultiByChannel(HitResults, RayOrigin, RayEnd, ECC_WorldStatic, CollisionParams);
 
+	DrawDebugLine(GetWorld(), RayOrigin, RayEnd, FColor::Yellow, false, -1, 0, 1);
+
+	//FString Msg = FString::Printf(TEXT("%f %f %f"), RayOrigin.X, RayOrigin.Y, RayOrigin.Z);	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, Msg);
 	
-	if (bHit) 
+	if (bHit && VisualComponent != nullptr)
 	{
-		FVector wantedPos(0, 0, HitResult.Distance);
-		VisualComponent->SetRelativeLocation(wantedPos);
+		for (FHitResult Hit : HitResults)
+		{
+			//UE_LOG(LogTemp, Log, TEXT("Moving component"));
+			UE_LOG(LogTemp, Log, TEXT("%f"), Hit.Distance);
+			
+			FVector wantedPos(0, 0, Hit.Distance);
 
-		return HitResult.Distance;
+			// Set floating object to surface position
+			VisualComponent->SetRelativeLocation(wantedPos);
+			
+			//OwnerActor->SetActorLocation(wantedPos);
+			if (Hit.GetActor()->GetName() == TEXT("OceanMesh"))
+			{
+				
+			}
+		}
+	}
+	else if (!bHit)
+	{
+		UE_LOG(LogTemp, Log, TEXT("LineTrace didn't hit anything"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("No Visual Component"));
 	}
 	
 	return 0;
